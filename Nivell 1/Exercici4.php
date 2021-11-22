8207864e2fb0f4fbebeee6c24450d839111bbaa5
David Abanades <duckydave.abanades@gmail.com>
2021-11-22 19:51:48 +0100

    Nivell 1 - Exercici 4 v2.0

--- Nivell 1/Exercici4.php
+++ Nivell 1/Exercici4.php
@@ -1,9 +1,15 @@
 <?php
-function compteEnrere($num = 10) {
+function compteEnrere($num) {
     for ($i = 2; $i <= $num; $i += 2) {
         echo $i . "<br>";
     }
 }
 
-compteEnrere();
+/* si el nombre de paràmetres es igual a 1 (Exercic1.php) */
+if($argc == 1) {
+    compteEnrere(10);
+/* en cas contrari ens avisa per pantalla*/
+} else {
+    echo "Per executar el compte enrere no has d'introduir cap numero separat per espais (el valor per defecte es 10" . "\n";
+}
 ?>
