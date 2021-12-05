43c564ffc34934b3f2c8bb59ba7dd762814ce3c4
Ducky Dave <duckydave.abanades@gmail.com>
2021-12-05 18:33:10 +0100

    Nivell 2 - Exercici 2 v3.0

--- Nivell 2/Exercici2.php
+++ Nivell 2/Exercici2.php
@@ -1,17 +1,12 @@
 <?php
     function preuTrucada (int $durada) {
         $cost = 10;
-        if ($durada >= 180) {
-            $cost += (($durada - 180) * 5);
+        if ($durada >= 3) {
+            $cost += (($durada - 3) * 5);
         }
         return $cost;
     }
-
-    function segonsTotals(int $minuts, int $segons) {
-        $segonsTotals = (60 * $minuts) + $segons;
-        return $segonsTotals;
-    }
-
+    
     function imprimirText($cost) {
         echo ("Cost de la trucada: ");
         if ($cost < 100) {
@@ -21,19 +16,19 @@
         }
     }
 
-    /* si el nombre de paràmetres es igual a 3 (Exercic2.php 2 45) i els segons no passen de 59 */
+    /* si el nombre de parametres es igual a 3 ("Nivell 2/Exercici2.php" 2 45) i els segons no passen de 59 */
     if ($argc == 3 && $argv[2] <= 59) {
-        /* la durada es converteix a segons */
-        $durada = segonsTotals($argv[1], $argv[2]);
+        // arrodonim la durada al minut sencer seguent
+        $durada = $argv[1] + 1;  
         imprimirText(preuTrucada($durada));
-    /* si el nombre de paràmetres es igual a 2 (Exercic2.php 6) */
+    /* si el nombre de parametres es igual a 2 ("Nivell 2/Exercici2.php" 6) */
     } else if ($argc == 2) {
-        /* la durada es converteix en segons */
-        $durada = segonsTotals($argv[1], 0);
+        // durada en minuts exactes
+        $durada = $argv[1];
         imprimirText(preuTrucada($durada));
     /* en cas d'error avisem per pantalla */
     } else {
-        echo ("Per calcular el cost de la trucada s'ha d'indicar la seva durada en minuts i segons separats per un espai en blanc" ."\n");
-        echo ("Per exemple: php Exercici2.php 3 48". "\n");
+        echo ("Per calcular el cost de la trucada s'ha d'indicar la seva durada en minuts i segons separats per un espai en blanc. " ."\n");
+        echo ("Per exemple: php \"Nivell 2/Exercici2.php\" 3 48" . "\n");
     }
 ?>
